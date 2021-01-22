d380c201504a3485c93ec5353c3397da4f766003
rikuyu <rikuyu5884@gmail.com>
2021-01-22 16:17:03 +0900

    add: enemy-level, refact

--- report1.c
+++ report1.c
@@ -1,22 +1,14 @@
 #include "gba1.h"
 
 //プロトタイプ宣言
-void draw_bullet(hword, hword, hword, int);			 //円を表示
-void draw_square(hword, hword, hword);				 //四角形を表示
-void shoot(hword, hword);							 //弾を出す関数
-void draw_enemy(hword ix, hword iy, hword i_color);	 //敵を表示
-void draw_player(hword px, hword py, hword p_color); //プレイヤーを表示
-void display_time(hword val);						 //timeを表示
+void display_bullet(hword, hword, hword, int);
+void transition_screen(hword, hword, hword);
+void shoot(hword, hword);
+void display_player(hword px, hword py, hword color);
+void display_block(hword bx, hword by, hword color);
+void display_time(hword val);
 
-// //プロトタイプ宣言
-// void draw_bullet(hword, hword, hword, int);			 //円を表示
-// void draw_square(hword, hword, hword);				 //四角形を表示
-// void shoot(hword, hword);							 //弾を出す関数
-// void draw_enemy(hword ix, hword iy, hword i_color);	 //敵を表示
-// void draw_player(hword px, hword py, hword p_color); //プレイヤーを表示
-// void display_time(hword val);						 //timeを表示
-
-unsigned char enemy[10][10] = {
+unsigned char block[10][10] = {
 	{1, 1, 1, 1, 1, 1, 1, 1, 1, 1},
 	{1, 1, 1, 1, 1, 1, 1, 1, 1, 1},
 	{1, 1, 1, 1, 1, 1, 1, 1, 1, 1},
@@ -44,17 +36,20 @@ unsigned char player[10][10] = {
 int main(void)
 {
 	hword key;
-	// *((hword *)0x04000000) = 0x0F03;
-	// hword *ptr2 = (hword *)0x04000130;
-	/* 画面初期化ルーチン */
+	/* 画面初期化*/
 	*((hword *)IOBASE) = 0xF03;
 
-	hword player_x = 120; //プレイヤーの初期のx座標
-	hword player_y = 140; //プレイヤーの初期のy座標
-	hword enemy_x = 0;	  //敵の初期のx座標
-	hword enemy_y = 40;	  //敵の初期のy座標
-	hword start_flag = 1; //GAME STARTのwhile文の真理値
-	hword point = 0;	  //初めの点数
+	hword player_x = 120;
+	hword player_y = 140;
+	hword block_x = 0;
+	hword block_y = 40;
+	hword start_flag = 1;
+	hword point = 0;
+	hword clear_time;
+	hword display_width = 240;
+	hword display_height = 160;
+	hword enemy_color = YELLOW;
+	hword enemy_level = 1;
 
 	volatile unsigned short time;
 
@@ -72,11 +67,10 @@ int main(void)
 
 	while (1)
 	{
-
 		/* キー入力 */
 		key = ~(*(hword *)KEY_STATUS) & KEY_ALL;
 
-		//仕様を表示
+		//操作説明を表示
 		locate(10, 3);
 		prints("Shoot Game");
 
@@ -96,22 +90,15 @@ int main(void)
 		prints("A");
 
 		if (key == KEY_B)
-		{ //	Bが押されたらwhile文を抜け出す
+		{
 			break;
 		}
 	}
 
-	draw_square(240, 160, 0x0000); //画面を一旦黒に塗りつぶす
+	//操作説明画面からの遷移
+	transition_screen(display_width, display_height, BLACK);
 	locate(1, 1);
-	prints("Point ="); //点数表示
-
-	// for (j = 0; j < 480; j++)
-	// {
-	// 	draw_point(j, 16, BGR(0x1F, 0x00, 0x1F)); //マゼンタのラインを表示
-	// }
-
-	// *((unsigned short *)0x0400010E) = 0x0000; // タイマ3 制御設定（タイマON，割込みOFF，カスケードON，プリスケーラなし）
-	// *((unsigned short *)0x0400010E) = 0x0084; // タイマ3 制御設定（タイマON，割込みOFF，カスケードON，プリスケーラなし）
+	prints("Point =");
 
 	locate(19, 3);
 	prints("Time = ");
@@ -120,124 +107,118 @@ int main(void)
 	prints("Goal Point = 5");
 	while (start_flag)
 	{
-
 		/* タイマ3 カウント値読込み */
 		time = *((unsigned short *)0x0400010C);
 
-		/* time 表示位置設定 */
 		locate(26, 3);
 		display_time(time);
 
 		/* キー入力 */
-
 		key = ~(*(hword *)KEY_STATUS) & KEY_ALL;
 
-		draw_player(player_x, player_y, BLUE); //プレイヤーを画面に表示
+		display_player(player_x, player_y, BLUE);
 
-		draw_enemy(enemy_x, enemy_y, 0x0000); //敵を画面に表示
-		enemy_x += 4;						  //敵の移動処理
+		display_block(block_x, block_y, BLACK);
+		block_x += 4;
 
-		if (enemy_x == 240)
-		{ //右端までいったら下の段に降りる処理
-			// enemy_y += 19;
-			enemy_x = 0;
-		}
-		else if (enemy_y >= 133)
+		switch (point)
 		{
-			start_flag = 0; //while文を抜け出す処理
+		case 0:
+			enemy_color = YELLOW;
+			break;
+		case 1:
+			enemy_color = GREEN;
+			break;
+		case 2:
+			enemy_color = CYAN;
+			break;
+		case 3:
+			enemy_color = MAGENTA;
+			break;
+		case 4:
+			enemy_color = RED;
+			break;
+		default:
+			break;
 		}
-
-		draw_enemy(enemy_x, enemy_y, YELLOW);
-
-		locate(9, 1);
-		printn(point); //点数表示
+		if (block_x == 240)
+		{
+			block_x = 0;
+		}
+		display_block(block_x, block_y, enemy_color);
 
 		switch (key)
 		{
-
 		case KEY_LEFT:
-			if (player_x == 0)
-			{ //左端で左が押されたら何もしない
-			}
-			else
+			if (player_x != 0)
 			{
-				draw_player(player_x, player_y, 0x0000); //色を一度背景色に
-				player_x -= 3;							 //x座標を左に1ずらす
-				draw_player(player_x, player_y, BLUE);	 //再び色を白に戻す
+				display_player(player_x, player_y, BLACK);
+				player_x -= 3;
+				display_player(player_x, player_y, BLUE);
 			}
 			break;
 
 		case KEY_RIGHT:
-			if (player_x == 216)
-			{ //右端で右が押されたら何もしない
-			}
-			else
+			if (player_x != 231)
 			{
-				draw_player(player_x, player_y, 0x0000); //色を一度背景色に
-				player_x += 3;							 //x座標を右に1ずらす
-				draw_player(player_x, player_y, BLUE);	 //再び色を白に戻す
+				display_player(player_x, player_y, BLACK);
+				player_x += 3;
+				display_player(player_x, player_y, BLUE);
 			}
 			break;
 
 		case KEY_A:
 			shoot(player_x, player_y - 3);
-			//敵とのx座標のずれが±5の場合(弾が当たった場合)
-			if (player_x + 12 > enemy_x && player_x + 12 < enemy_x + 24)
+			if (player_x + 12 > block_x && player_x + 12 < block_x + 20)
 			{
-				draw_enemy(enemy_x, enemy_y, 0x0000);
-				enemy_x = 0;
-				enemy_y = 40;
-				point += 1; //点数加算
+				display_block(block_x, block_y, BLACK);
+				block_x = 0;
+				block_y = 40;
+				point += 1;
 			}
 			break;
 		}
-
+		locate(9, 1);
+		printn(point);
 		int i;
-		for (i = 0; i < 20000; i++)
+		for (i = 0; i < 30000; i++)
 		{
-		} //時間調節
+		}
 		if (point == 5)
 		{
-
-			locate(13, 8);
+			clear_time = time;
+			locate(13, 7);
 			prints("Clear");
-
-			locate(11, 11);
+			locate(11, 9);
 			prints("Thank You");
-
+			locate(11, 11);
+			prints("Claer Time");
+			locate(13, 13);
+			display_time(clear_time);
+			locate(17, 13);
+			prints("ms");
 			while (1)
 				;
 		}
 	}
-
-	// draw_square(240, 160, BGR(0x1F, 0x00, 0x00)); //GAMEOVERを表示
-	// locate(10, 10);
-	// prints("Game Over...");
-	prints("F I N I S H");
-	draw_enemy(116, 56, BLUE);
-
-	/* 無限ループによるプログラム停止 */
 	while (1)
 		;
-
 	return 0;
 }
 
-//四角形を表示
-void draw_square(hword x, hword y, hword color)
+void transition_screen(hword x, hword y, hword color)
 {
-	hword x1, y1;
-	for (x1 = 0; x1 < x; x1++)
+	hword point_x, point_y;
+	for (point_x = 0; point_x < x; point_x++)
 	{
-		for (y1 = 0; y1 < y; y1++)
+		for (point_y = 0; point_y < y; point_y++)
 		{
-			draw_point(x1, y1, BGR(0x00, 0x00, 0x00));
+			draw_point(point_x, point_y, BLACK);
 		}
 	}
 }
 
-//円を表示
-void draw_bullet(hword a, hword b, hword color, int r)
+void display_bullet(hword a, hword b, hword color, int r)
 {
 	unsigned short *ptr;
 	hword x, y;
@@ -252,50 +233,46 @@ void draw_bullet(hword a, hword b, hword color, int r)
 		}
 	}
 }
-//弾をうつ関数
-void shoot(hword x, hword y)
+
+void shoot(hword x, hword range_height)
 {
-	while (y > 40)
-	{									  //y座標が最上位に来るまで
-		draw_bullet(x + 4, y, 0x0000, 2); //背景色に塗りつぶす
-		y--;							  //y座標を1ドット上に移動
-		draw_bullet(x + 4, y, BLUE, 2);	  //弾を表示
-		//ゆっくりにする為の処理
+	while (range_height > 40)
+	{
+		display_bullet(x + 4, range_height, BLACK, 2);
+		range_height--;
+		display_bullet(x + 4, range_height, BLUE, 2);
 		int i;
-		for (i = 0; i < 50; i++)
+		for (i = 0; i < 140; i++)
 		{
 		}
 	}
-	draw_bullet(x + 12, y, 0x0000, 2);
+	display_bullet(x + 12, range_height, BLACK, 2);
 }
-//敵を表示
-void draw_enemy(hword ix, hword iy, hword i_color)
-{
-	int n, m;
 
-	for (n = 0; n < 10; n++)
+void display_player(hword px, hword py, hword color)
+{
+	int x, y;
+	for (x = 0; x < 10; x++)
 	{
-		for (m = 0; m < 10; m++)
+		for (y = 0; y < 10; y++)
 		{
-			if (enemy[n][m] == 1)
+			if (player[x][y] == 1)
 			{
-				draw_point((m + ix), (n + iy), i_color);
+				draw_point((y + px), (x + py), color);
 			}
 		}
 	}
 }
-//プレイヤーを表示
-void draw_player(hword px, hword py, hword p_color)
+void display_block(hword bx, hword by, hword color)
 {
-	int n, m;
-
-	for (n = 0; n < 10; n++)
+	int x, y;
+	for (x = 0; x < 10; x++)
 	{
-		for (m = 0; m < 10; m++)
+		for (y = 0; y < 10; y++)
 		{
-			if (player[n][m] == 1)
+			if (block[x][y] == 1)
 			{
-				draw_point((m + px), (n + py), p_color);
+				draw_point((y + bx), (x + by), color);
 			}
 		}
 	}
@@ -303,28 +280,17 @@ void draw_player(hword px, hword py, hword p_color)
 
 void display_time(hword val)
 {
-
-	/* 引数を10進数として表示する関数（最大値16ビット）*/
-
 	byte char_data[] = "0123456789";
 	byte buf[6];
 	hword tmp;
-	int i;
-
-	/* 入力値valの桁数設定 */
-	i = 3;
-
-	/* 文字列の最後にNULLコード挿入 */
+	int i = 3;
 	buf[i + 1] = 0;
-
-	/* 最下位桁の文字（10で割った余り）を挿入し，入力値を10で割る */
 	for (; i >= 0; i--)
 	{
 		buf[i] = char_data[mod(val, 10)];
 		val = div(val, 10);
 	}
 
-	/* 文字列全体を表示 */
 	prints(buf);
 
 	return;
